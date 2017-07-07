41f6d4eaacf92ccedbf7594aa323415f6f49c429
Efosa Okpugie <efosaokpugie@gmail.com>
2017-07-07 21:40:35 +0100

    remodified

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const express = require('express');
+var express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
@@ -10,78 +8,57 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
+const MongoStore = require("connect-mongo")(session);
 
-mongoose.connect('mongodb://efosaokpugie:swampious88@ds143231.mlab.com:43231/shoppp');
-const db = mongoose.connection;
+//configuring environmental variiables
 
-const routes = require('./routes/index');
-const users = require('./routes/users');
 
 // Init App
-const app = express();
+//connect to mongoDB
 
-// View Engine
+mongoose.connect("mongodb://efosaokpugie:swampious88@ds143231.mlab.com:43231/shoppp");
+const db = mongoose.connection;
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
-// BodyParser Middleware
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));
-
-// Passport init
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Express Validator
-app.use(expressValidator({
-  errorFormatter: (param, msg, value)=> {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-// Connect Flash
-app.use(flash());
-
-// Global Vars
-app.use((req, res, next)=> {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  next();
-});
-
-
-
-app.use('/', routes);
+app.use('/', index);
 app.use('/users', users);
 
-// Set Port
-app.set('port', (process.env.PORT || 3000));
-
-app.listen(app.get("port"),()=>{
-  console.log("Server running on port"+" "+app.get("port"))
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -11,36 +11,6 @@ router.get("/",(req,res)=>{
 
 router.get("/home",(req,res)=>{
 	res.render("index")
-})
-const convertCurrencies = (currencyToConvert,convertToCurrency,Amount)=>{
-	let naira = {"dollar":0.0032,"euro":0.0028,"yen":0.36,"pounds":0.0024}
-	let dollar = {"naira":315.25,"euro":0.88,'yen':112.04,"pounds":0.77}
-	let pounds = {"yen":145.36,"euro":1.14,"dollar":1.30,"naira":409.10}
-	let euro = {"yen":127.73,"pounds":0.88,"dollar":1.14,"naira":359.48}
-	let yen = {"pounds":0.0069,"dollar":0.0089,"naira":2.82,"euro":0.0078}
+});
 
-	if(currencyToConvert === "naira"){
-		return naira[convertToCurrency] * parseFloat(Amount)
-	}
-	else if(currencyToConvert === "dollar"){
-		return dollar[convertToCurrency] * parseFloat(Amount)
-	}
-	else if(currencyToConvert === "pounds"){
-		return pounds[convertToCurrency] * parseFloat(Amount)
-	}
-	else if (currencyToConvert === "euro"){
-		return euro[convertToCurrency] * parseFloat(Amount)
-	}
-	else{
-		return yen[convertToCurrency] * parseFloat(Amount)
-	}
-}
-router.get("/convertCurrency",(req,res)=>{
-	a = req.body.amount
-	b = req.body.myselect
-	c = req.body.myconvert
-	console.log(convertCurrencies(a,b,c))
-	res.redirect("/home")
-})
-
-module.exports = router
+module.exports = router;
